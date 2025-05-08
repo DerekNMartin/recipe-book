f94bdf736afcb953cde61570f8b3a0b15e681b70
Derek Martin <derek.n.martin@gmail.com>
2025-05-08 12:42:44 -0400

    feat: remove vercel speed insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { SpeedInsights } from '@vercel/speed-insights/nuxt';
 import { Analytics } from '@vercel/analytics/nuxt';
 import useAuth from '@/composables/useAuth';
 
@@ -8,14 +7,11 @@ const isAuthModalVisible = ref(!isAuthenticated.value);
 </script>
 
 <template>
-  <main
-    class="sm:p-12 p-6 grid grid-rows-[auto,1fr,auto] min-h-screen bg-amber-50 print:p-0"
-  >
+  <main class="sm:p-12 p-6 grid grid-rows-[auto,1fr,auto] min-h-screen bg-amber-50 print:p-0">
     <LayoutHeader />
     <slot />
     <LayoutFooter />
     <AuthModal v-model:visible="isAuthModalVisible" />
-    <SpeedInsights />
     <Analytics />
     <BaseToast />
   </main>
