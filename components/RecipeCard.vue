3665441cafb67d6900dd02f0163d94dc44dd6e80
Derek Martin <derek.n.martin@gmail.com>
2025-05-29 10:22:57 -0400

    style: update card

--- components/RecipeCard.vue
+++ components/RecipeCard.vue
@@ -7,28 +7,28 @@ const props = defineProps<{ recipe: Recipe | RecipesResponse[number] }>();
 
 <template>
   <article
-    class="rounded-xl border-2 border-solid border-primary-700 overflow-hidden flex flex-col justify-end h-full max-h-96 transition-transform aspect-[4/3] group hover:rotate-1 duration-300 origin-bottom-right"
+    class="rounded-2xl overflow-hidden flex flex-col justify-end h-full max-h-96 transition-transform aspect-[4/3] group hover:rotate-1 duration-300 origin-bottom-right bg-primary-700"
   >
     <NuxtLink
       :to="{ name: 'recipe-recipeId', params: { recipeId: recipe.id } }"
-      class="w-full h-full"
+      class="w-full h-full p-3 overflow-hidden"
     >
       <NuxtImg
         :src="recipe.image_url || undefined"
         fit="cover"
         width="500"
         format="webp"
-        class="object-cover w-full h-full group-hover:scale-105 transition-transform duration-300"
+        class="object-cover w-full h-full group-hover:scale-105 transition-transform duration-300 rounded-xl"
         :alt="recipe.title || undefined"
       />
     </NuxtLink>
-    <div class="flex flex-col gap-1 p-4 bg-white z-10">
+    <div class="flex flex-col gap-1 px-4 pb-4 bg-primary-700 z-10">
       <NuxtLink :to="{ name: 'recipe-recipeId', params: { recipeId: recipe.id } }">
-        <h3 class="text-2xl">{{ recipe.title }}</h3>
+        <h3 class="text-xl text-white">{{ recipe.title }}</h3>
       </NuxtLink>
       <span class="flex items-center gap-2">
-        <span class="w-6 h-[2px] bg-primary-700 block rounded" />
-        <p class="text-sm text-neutral-700 font-medium">By {{ recipe.author }}</p>
+        <span class="w-6 h-[2px] bg-white block rounded" />
+        <p class="text-sm font-medium text-white">By {{ recipe.author }}</p>
       </span>
     </div>
   </article>
